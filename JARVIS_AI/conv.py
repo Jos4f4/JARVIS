2b948cb1264a6007f326e5c51c856af12c28b760
Josaf4 <josafa.soares2909@gmail.com>
2025-09-22 00:05:06 -0400

    Implemented more functions in the projetc

---
+++ JARVIS_AI/conv.py
@@ -0,0 +1,5 @@
+random_text = [
+    "Coll, I'm on it sir",
+    "okay sir, I'm working on it",
+    "Just a second sir",
+]
--- JARVIS_AI/main.py
+++ JARVIS_AI/main.py
@@ -1,7 +1,11 @@
 import pyttsx3
+import speech_recognition as sr
+import keyboard
 
 from datetime import datetime
 from decouple import config
+from conv import random_text
+from random import choice
 
 engine = pyttsx3.init('sapi5')
 engine.setProperty('volume', 1.5)
@@ -26,5 +30,41 @@ def greet_me():
         speak(f"Good evening {USER}")
     speak(f"I am {HOSTNAME}. How may i assist you? { USER}")
 
+listening = False
+
+def start_listening():
+    global listening
+    listening = True #I'm stopped minute 20
+
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing....")
+        queri = r.recognize_google(audio, language = 'en-in')
+        print(queri)
+        if not 'stop' in queri or 'exit' in queri:
+            speak(choice(random_text))
+        else:
+            hour = datetime.now().hour
+            if hour >= 21 and hour < 6:
+                speak("Good night sir, take care!")
+            else:
+                speak("Have a good day sir")
+            exit()
+    except Exception:
+        speak("Sorry I couldn't understabd. Can you please repeat that?")
+        queri = 'None'
+
+    return queri
+
 if __name__ == '__main__':
     greet_me()
+    while True:
+        query = take_command().lower()
+        if "how are you" in query:
+            speak("I am absolutely fine sir. What about you")
